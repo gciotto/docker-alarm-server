47fe8fac4e01dc38ad1bb27c1645548993cd9a1b
Gustavo Ciotto <gustavociotto@gmail.com>
2017-03-09 17:45:42 -0300

    minor improvements

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ source env-vars.sh
 
 CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
-if [ ! -z "$CONTAINERS" ]; then
+if [ ! -z ${CONTAINERS:+x} ]; then
+    docker stop ${DOCKER_RUN_NAME}
     docker rm ${DOCKER_RUN_NAME}
 fi
 
@@ -16,3 +17,4 @@ docker run -d --name=${DOCKER_RUN_NAME} --network=${NETWORK_ID} \
     --entrypoint=${ALARM_FOLDER}/beast-alarm-server-4.1.1/scripts/${ALARM_START_SCRIPT} \
     ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME} start
 
+docker logs -f ${DOCKER_RUN_NAME}
