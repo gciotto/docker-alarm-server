811587d259da3b5a5569154e0e3df6bbe249bfef
Gustavo Ciotto <gustavociotto@gmail.com>
2017-03-10 08:55:02 -0300

    container run name updated

--- env-vars.sh
+++ env-vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DOCKER_MANTAINER_NAME=gciotto
-DOCKER_NAME=alarm-server-service
-DOCKER_RUN_NAME=alarm-server-docker-container
+DOCKER_NAME=alarm-server
+DOCKER_RUN_NAME=alarm-server
 
 GITHUB_ALARM_REPO=https://github.com/ControlSystemStudio/org.csstudio.alarmserver.product.git
 GITHUB_ALARM_BRANCH=master
