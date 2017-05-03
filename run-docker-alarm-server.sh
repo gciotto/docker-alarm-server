451fc0fa959bc77ca134b88d9613a1b3f7c2c49f
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:24:09 -0300

    commentaries and verbose commands

--- run-docker-alarm-server.sh
+++ run-docker-alarm-server.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-set -a
-set -e
-set -u
+#
+# An auxiliary script to start a new Docker container. It should be used to test the 
+# respective image. To deploy, use the composed repository, available in
+# https://github.com/lnls-sirius/docker-alarm-composed .  
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+. ./env-vars.sh
 
-source env-vars.sh
-
+# Verifies if a container with the same name is already running.
 CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
 if [ ! -z ${CONTAINERS:+x} ]; then
-    docker stop ${DOCKER_RUN_NAME}
-    docker rm ${DOCKER_RUN_NAME}
+
+    echo "A container with the name ${DOCKER_RUN_NAME} is already running..."
+
+    # Stops running container and deletes it.
+    echo "Executing 'docker stop ${DOCKER_RUN_NAME}' ..."
+    docker stop ${DOCKER_RUN_NAME} &> /dev/null
+
+    echo "Executing 'docker rm ${DOCKER_RUN_NAME}' ..."
+    docker rm ${DOCKER_RUN_NAME} &> /dev/null
 fi
 
+# Runs a new instance of the Docker image
 docker run -d --name=${DOCKER_RUN_NAME} --network=${NETWORK_ID} \
     ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}
 
