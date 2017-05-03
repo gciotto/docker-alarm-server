33472b3f5b0270ad52e9d8b149d797fa3f77770b
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:26:13 -0300

    removed variables already defined in Dockerfile

--- env-vars.sh
+++ env-vars.sh
@@ -4,10 +4,4 @@ DOCKER_MANTAINER_NAME=gciotto
 DOCKER_NAME=alarm-server
 DOCKER_RUN_NAME=alarm-server
 
-GITHUB_ALARM_REPO=https://github.com/ControlSystemStudio/org.csstudio.alarmserver.product.git
-GITHUB_ALARM_BRANCH=master
-
-ALARM_FOLDER=/opt/lnls-alarm-server
-ALARM_START_SCRIPT=docker-alarm-init
-
 NETWORK_ID=alarm-server-internal-network
