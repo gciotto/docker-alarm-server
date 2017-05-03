14a4d8b1453fceab880497dc7ab88c7a21f2f75f
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:25:26 -0300

    commands moved from Dockerfile to setup-beast.sh

--- scripts/setup-beast.sh
+++ scripts/setup-beast.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-. ./env-vars.sh
+#
+# A script to clone and build the alarm server application.
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory
+#
 
-git clone --branch=${GITHUB_ALARM_BRANCH} ${GITHUB_ALARM_REPO}
+TMP_FOLDER=org.csstudio.alarmserver.product
 
-cd org.csstudio.alarmserver.product/
+git clone --branch=${GITHUB_ALARM_BRANCH} ${GITHUB_ALARM_REPO} ${TMP_FOLDER}
 
-mvn clean install
+# Compile alarm server via Maven
+( cd ${TMP_FOLDER} && mvn clean install )
+
+# Copy and remove compiled binaries
+cp -R ./${TMP_FOLDER}/repository/target/products/beast-alarm-server/linux/gtk/x86_64/${ALARM_VERSION} ${ALARM_FOLDER}
+rm -r ./${TMP_FOLDER}
